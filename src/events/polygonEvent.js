d33258734a08ea3aae25e04a15a1b115d0a64b46
mufraswid <13518147@std.stei.itb.ac.id>
2021-02-19 17:08:00 +0700

    Creating polygon outline process done, need fix on Polygon constructor

--- src/events/polygonEvent.js
+++ src/events/polygonEvent.js
@@ -20,18 +20,19 @@ export const mouseDownPolygonEvent = (event) => {
         // Alternating for the polygon construction: after 1st vertex, whenever clicked means storing new vertex to array
         if (Context.getInstance().getNPolygon() > 1) {
             const lastIndex = Context.getInstance().getShapes().length - 1;
-            const nextVertex = Context.getInstance().getShapes()[lastIndex].getPoints[0];
+            const nextVertex = Context.getInstance().getShapes()[lastIndex].getPoints()[1];
             Context.getInstance().addPolygonVertex(nextVertex);
-            console.log(Context.getInstance().getPolygonVertices());
         }
 
+        /* Threshold point block begin */
         let minIdx = -1;
         let min = 999;
         let nPoint = -1;
 
         const shapes = Context.getInstance().getShapes();
+        const shapelen = shapes.length;
         shapes.forEach((shape, idx) => {
-            if (shape.getShapeType() === "line") {
+            if (shape.getShapeType() === "line" && idx < shapelen - 1) {
                 const points = shape.getPoints();
                 for (let i = 0; i < points.length; i++) {
                     const distance = euclidianDistance(points[i], vec2(x, y));
@@ -44,24 +45,27 @@ export const mouseDownPolygonEvent = (event) => {
             }
         });
 
-        console.log(Context.getInstance().getNPolygon());
         // Get first point in the first line
         let lenShapes = Context.getInstance().getShapes().length
         // Get number of polygon vertices
         let nPolygon = Context.getInstance().getNPolygon();
         // Get the first point in the polygon
-        let firstPoint = Context.getInstance().getShapes()[lenShapes - nPolygon][0];
-    
+        let firstPoint = Context.getInstance().getShapes()[lenShapes - nPolygon + 1].getPoints()[0];
 
+        console.log(Context.getInstance().getPolygonVertices());
+        console.log(firstPoint);
+    
         // threshold passed, found target
         if (minIdx != -1 && min != 999) {
             // Current point
-            curPoint = Context.getInstance().getShapes()[minIdx].getPoints();
+            curPoint = Context.getInstance().getShapes()[minIdx].getPoints()[0];
             // if equals the first point of the line, then polygon complete.
-            if (point[0] === firstPoint[0] && point[1] === firstPoint[1]) {
+            if (curPoint[0] === firstPoint[0] && curPoint[1] === firstPoint[1]) {
+                console.log("IWAW MASHOK");
                 Context.getInstance().finishPolygon();
             }
         }
+        /* Threshold point block end */
     }
     
     if (!curPoint) {
@@ -69,13 +73,11 @@ export const mouseDownPolygonEvent = (event) => {
     }
 
     if (!Context.getInstance().isPolygonFinished()) { // If not finished, draw a line
-        Context.getInstance().changeMode("line");
-        Context.getInstance().addShape(new Line(vec2(x, y), vec2(x, y), Context.getInstance().getColor()));
-
         if (Context.getInstance().getNPolygon() == 0) {
+            Context.getInstance().changeMode("line");
+            Context.getInstance().addShape(new Line(vec2(x, y), vec2(x, y), Context.getInstance().getColor()));
             Context.getInstance().addPolygonVertex(vec2(x,y));
         }
-        
     } else { // If finished, then pop all of the lines and draw the polygon
         for (var i = 1; i <= Context.getInstance().getNPolygon(); i++) {
             Context.getInstance().getShapes().pop();
@@ -94,18 +96,17 @@ export const mouseUpPolygonEvent = () => {
     // Alternating for the polygon construction: for the 1st vertex, whenever unclicked means storing new vertex to array
     if (Context.getInstance().getNPolygon() == 1) {
         Context.getInstance().addPolygonVertex(nextVertex);
-        console.log(Context.getInstance().getPolygonVertices());
     }
 
-    console.log(Context.getInstance().getNPolygon());
     Context.getInstance().changeMode("line");
     Context.getInstance().addShape(new Line(nextVertex, nextVertex, Context.getInstance().getColor()));
     render();
 }
 
 export const mouseMovingPolygonEvent = (event) => {
-    // Satiesfied condition when clicked on the first vertex or unclicked for the rest of the vertices
+    // Satiesfied condition when clicked on the first vertex OR unclicked for the rest of the vertices, all while the polygon is not finished
     let satisfied = (Context.getInstance().isClicked() && Context.getInstance().getNPolygon() == 1) || (!Context.getInstance().isClicked() && Context.getInstance().getNPolygon() > 1);
+    satisfied = satisfied && !Context.getInstance().isPolygonFinished();
     // Draw lines until it's finished
     if (satisfied) {
         const canvas = Context.getInstance().getCanvas();
